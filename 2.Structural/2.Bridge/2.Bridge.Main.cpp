debc75cece716348c8206580bd8f6add217667af
ilya.malyavskiy@gmail.com <ilya.malyavskiy@gmail.com>
2017-07-01 22:02:45 +0700

    The Bridge structure pattern added

---
+++ 2.Structural/2.Bridge/2.Bridge.Main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <memory>
+
+class Implementor
+{
+public:
+	virtual void Operation() = 0;
+};
+
+class ConcreteImplementorA : public Implementor
+{
+public:
+	void Operation() override
+	{
+		std::cout << "ConcreteImplementorA Operation" << std::endl;
+	}
+};
+
+class ConcreteImplementorB : public Implementor
+{
+public:
+	void Operation() override
+	{
+		std::cout << "ConcreteImplementorB Operation" << std::endl;
+	}
+};
+
+class Abstraction
+{
+protected:
+	// This aggregation is The Bridge itself.
+	std::shared_ptr<Implementor> m_pImplementor;
+
+public:
+	
+	std::shared_ptr<Implementor> getImplementor()
+	{
+		return m_pImplementor;
+	};
+
+	void setImplementor(const std::shared_ptr<Implementor>& a)
+	{
+		m_pImplementor = a;
+	}
+
+	virtual void Operation()
+	{
+		m_pImplementor->Operation();
+	}
+};
+
+class RefinedAbstraction : public Abstraction
+{
+public:
+	void Operation() override
+	{
+		m_pImplementor->Operation();
+	}
+};
+
+// MainApp test application
+void Main()
+{
+	std::shared_ptr<Abstraction> ab(new RefinedAbstraction());
+
+ 	// Set implementation and call
+	std::shared_ptr<ConcreteImplementorA> ImpA(new ConcreteImplementorA);
+ 	ab->setImplementor(std::static_pointer_cast<Implementor>(ImpA));
+ 	ab->Operation();
+ 
+ 	// Change implementation and call
+	std::shared_ptr<ConcreteImplementorB> ImpB(new ConcreteImplementorB);
+ 	ab->setImplementor(std::static_pointer_cast<Implementor>(ImpB));
+ 	ab->Operation();
+}
