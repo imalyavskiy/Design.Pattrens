27af673ee06706b443ecef9473bb0c586baded12
ilya.malyavskiy@gmail.com <ilya.malyavskiy@gmail.com>
2017-07-23 09:31:01 +0700

    Migrating to Qt. QTDIR_x32=D:\Qt\Qt_x32_5.6.2\5.6\msvc2015\ QTDIR_x64=D:\Qt\Qt_x64_5.6.2\5.6\msvc2015_64\

--- 4.Auxiliary/4.1.Model.View.Controller.cpp/Model.View.Controller.Main.cpp
+++ 4.Auxiliary/4.1.Model.View.Controller.cpp/Model.View.Controller.Main.cpp
@@ -1,12 +1,92 @@
 #include <iostream>
-#include <vector>
-#include <memory>
-#include <list>
-#include <random>
 
-#include "Model.View.Controller.Model.h"
+#include <QtWidgets\QApplication>
 
-int main(int argc, char* argv[])
+//
+
+#include <QtWidgets\QApplication>
+#include <QtWidgets\QHeaderView>
+#include <QtWidgets\QListView>
+#include <QtWidgets\QMainWindow>
+#include <QtWidgets\QTableView>
+#include <QtWidgets\QTreeView>
+#include <QtWidgets\QSizePolicy>
+
+int 
+AppMain(int argc, char* argv[])
 {
-	std::cout << "Hello I am Minesweeper!" << std::endl;
+	const int WINDOW_WIDTH = 800;
+	const int WINDOW_HEIGHT = 600;
+
+	QApplication app(argc, argv);
+	QMainWindow mainWindow;
+
+	//TODO: initialization
+
+ 	mainWindow.setCentralWidget(nullptr);
+	mainWindow.setWindowIcon(QPixmap(":/images/interview.png"));
+	mainWindow.setWindowTitle("Interview");
+	
+	mainWindow.setFixedWidth(WINDOW_WIDTH);
+	mainWindow.setFixedHeight(WINDOW_HEIGHT);
+
+	mainWindow.show();
+
+	return app.exec();
 }
+
+#ifdef _DEBUG
+int 
+main(int argc, char *argv[])
+{
+	int result = 0;
+	std::cout << "==== DEBUG CONSOLE STARTED ====" << std::endl;
+
+	//////////////////////////////////////////////////////////////////////////
+	Q_INIT_RESOURCE(interview);
+	result = AppMain(argc, argv);
+	//////////////////////////////////////////////////////////////////////////
+	
+	std::cout << "==== DEBUG CONSOLE STOPPED ====" << std::endl;
+	return result;
+}
+
+#else // defined NDEBUG
+#include <windows.h>
+
+int
+WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPreInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	int result = 0;
+	
+	int argc = 0;
+	char** argv = nullptr;
+
+	{
+		LPWSTR* lpArgv = CommandLineToArgvW(GetCommandLineW(), &argc);
+		argv = (char**)malloc(argc * sizeof(char*));
+		int size, i = 0;
+		for (; i < argc; ++i)
+		{
+			size = wcslen(lpArgv[i]) + 1;
+			argv[i] = (char*)malloc(size);
+			wcstombs(argv[i], lpArgv[i], size);
+		}
+		LocalFree(lpArgv);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	Q_INIT_RESOURCE(interview);
+	result = AppMain(argc, argv);
+	//////////////////////////////////////////////////////////////////////////
+
+	{
+		int i = 0;
+		for (; i < argc; ++i)
+			free(argv[i]);
+		free(argv);
+	}
+
+	return result;
+}
+#endif
